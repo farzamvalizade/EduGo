2d878ee6acdb42bf6b70ddf23066f1f6ac695a4d
Farzam Valizade <farzam.valizadeh.2020@gmail.com>
2026-01-02 15:20:09 +0330

    Navbar Added

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,62 @@
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav className="fixed bottom-0 left-0 right-0 flex flex-row-reverse justify-around items-center w-full h-16 bg-secondary-light border-t border-white/5 text-white z-50 px-2">
+      <Link to="/" className="flex items-center gap-4 px-4">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+        >
+          <path
+            fill="currentColor"
+            d="M10 19v-5h4v5c0 .55.45 1 1 1h3c.55 0 1-.45 1-1v-7h1.7c.46 0 .68-.57.33-.87L12.67 3.6c-.38-.34-.96-.34-1.34 0l-8.36 7.53c-.34.3-.13.87.33.87H5v7c0 .55.45 1 1 1h3c.55 0 1-.45 1-1"
+          />
+        </svg>
+      </Link>
+      <Link to="/subjects" className="flex items-center gap-4 px-4">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+        >
+          <path
+            fill="currentColor"
+            d="M9.5 16q-2.725 0-4.612-1.888T3 9.5t1.888-4.612T9.5 3t4.613 1.888T16 9.5q0 1.1-.35 2.075T14.7 13.3l5.6 5.6q.275.275.275.7t-.275.7t-.7.275t-.7-.275l-5.6-5.6q-.75.6-1.725.95T9.5 16m0-2q1.875 0 3.188-1.312T14 9.5t-1.312-3.187T9.5 5T6.313 6.313T5 9.5t1.313 3.188T9.5 14"
+          />
+        </svg>
+      </Link>
+      <Link to="/progress" className="flex items-center gap-4 px-4">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+        >
+          <path
+            fill="currentColor"
+            d="m12 17.275l-4.15 2.5q-.275.175-.575.15t-.525-.2t-.35-.437t-.05-.588l1.1-4.725L3.775 10.8q-.25-.225-.312-.513t.037-.562t.3-.45t.55-.225l4.85-.425l1.875-4.45q.125-.3.388-.45t.537-.15t.537.15t.388.45l1.875 4.45l4.85.425q.35.05.55.225t.3.45t.038.563t-.313.512l-3.675 3.175l1.1 4.725q.075.325-.05.588t-.35.437t-.525.2t-.575-.15z"
+          />
+        </svg>
+      </Link>
+      <Link to="/profile" className="flex items-center gap-4 px-4">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+        >
+          <path
+            fill="currentColor"
+            d="M7.5 6.5C7.5 8.981 9.519 11 12 11s4.5-2.019 4.5-4.5S14.481 2 12 2S7.5 4.019 7.5 6.5M20 21h1v-1c0-3.859-3.141-7-7-7h-4c-3.86 0-7 3.141-7 7v1z"
+          />
+        </svg>
+      </Link>
+    </nav>
+  );
+};
+
+export default Navbar;
--- frontend/src/pages/routes.tsx
+++ frontend/src/pages/routes.tsx
@@ -3,12 +3,14 @@ import { lazy } from "react";
 const Home = lazy(() => import("./Home.tsx"));
 const Login = lazy(() => import("./Login.tsx"));
 const Signup = lazy(() => import("./Signup.tsx"));
+const Profile = lazy(() => import("./Profile.tsx"));
 
 // Routes
 const routes: { path: string; element: any }[] = [
   { path: "/", element: <Home /> },
   { path: "/login", element: <Login /> },
   { path: "/signup", element: <Signup /> },
+  { path: "/profile", element: <Profile /> },
 ];
 
 export default routes;
