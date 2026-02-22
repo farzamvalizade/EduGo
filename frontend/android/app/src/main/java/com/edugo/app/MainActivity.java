e1e7c73da84c6b6a15d1397f64241dc4c8ecc344
Farzam Valizade <farzam.valizadeh.2020@gmail.com>
2026-02-22 19:06:02 +0330

    Change Frontend Api to backend host

---
+++ frontend/android/app/src/main/java/com/edugo/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.edugo.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- frontend/src/services/auth/authService.ts
+++ frontend/src/services/auth/authService.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import Cookies from "js-cookie";
 
-const API_URL = "http://localhost:8000/api";
+const API_URL = "https://farzam1389.pythonanywhere.com/api";
 
 const api = axios.create({
   baseURL: API_URL,
